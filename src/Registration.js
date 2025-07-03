b5ad97d920882500c358ea64ec6f1fe032c714d9
Iamkorir3 <korir5956@gmail.com>
2025-07-03 18:00:11 +0300

    working on forms

--- src/Registration.js
+++ src/Registration.js
@@ -1,5 +1,54 @@
-function Registration() {
-  return <div className="registration_container">Registration form</div>;
+import { useEffect } from "react";
+
+export default function Registration() {
+  const [userEmail, setUserEmail] = useEffect();
+  const [userPhoneNum, setUserPhoneNum] = useEffect();
+
+  function handleUserEmail(email) {
+    console.log("reacgede");
+    setUserEmail(email);
+  }
+
+  function handleUserPhoneNum(number) {
+    console.log("reacgede");
+    setUserEmail(number);
+  }
+
+  return (
+    <div className="registration_container">
+      <Form
+        userEmail={userEmail}
+        setUserEmail={handleUserEmail}
+        userPhoneNum={userPhoneNum}
+        setUserPhoneNum={handleUserPhoneNum}
+      />
+    </div>
+  );
 }
 
-export default Registration;
+function Form({ userEmail, setUserEmail, userPhoneNum, setUserPhoneNum }) {
+  return (
+    <form className="regis_form">
+      <label for email>
+        Enter your email
+      </label>
+      <input
+        type="email"
+        value={userEmail}
+        onChange={(e) => setUserEmail(e.target.value)}
+      />
+
+      <label for phoneNUmber>
+        Enter your Phone number
+      </label>
+      <input
+        type="number"
+        name="phoneNUmber"
+        value={userPhoneNum}
+        onChange={(e) => setUserPhoneNum(e.target.value)}
+      />
+
+      <button type="submit">submit</button>
+    </form>
+  );
+}
