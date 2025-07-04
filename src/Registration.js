f9977e989ff496b26a2bcc1340a98f4955af0e1d
Iamkorir3 <korir5956@gmail.com>
2025-07-05 02:11:31 +0300

    done with registration form

--- src/Registration.js
+++ src/Registration.js
@@ -18,11 +18,24 @@ export default function Registration() {
 function Form({ Onsubmission }) {
   const [userEmail, setUserEmail] = useState("");
   const [userPhoneNum, setUserPhoneNum] = useState("");
+  const [noData, setnoData] = useState(false);
+  const [noNumber, setnoNumber] = useState(false);
+
+  // function hi() {
+  //   userPhoneNum.length > 0 ? setnoNumber(true) : setnoNumber(false);
+  // }
 
   function handleOnsubmit(e) {
     e.preventDefault();
 
-    if (!userEmail || !userPhoneNum) return;
+    if (!userEmail) {
+      setnoData(true);
+    }
+    if (!userPhoneNum) {
+      setnoNumber(true);
+
+      return;
+    }
 
     const createUser = {
       userEmail,
@@ -32,23 +45,47 @@ function Form({ Onsubmission }) {
     Onsubmission(createUser);
   }
   return (
-    <form className="regis_form" onSubmit={handleOnsubmit}>
-      <label>Enter your email</label>
-      <input
-        type="email"
-        value={userEmail}
-        onChange={(e) => setUserEmail(e.target.value)}
-      />
+    <>
+      <RegistrationLogo />
+      <form className="regis_form" onSubmit={handleOnsubmit}>
+        <label>Email Address</label>
+        <input
+          type="email"
+          value={userEmail}
+          onChange={(e) => setUserEmail(e.target.value)}
+        />
+        <p className={`error_p ${noData ? "" : "err"}`}>Email valid email</p>
 
-      <label>Enter your Phone number</label>
-      <input
-        type="number"
-        name="phoneNUmber"
-        value={userPhoneNum}
-        onChange={(e) => setUserPhoneNum(e.target.value)}
-      />
+        <label>Phone number</label>
+        <input
+          type="country"
+          name="phoneNUmber"
+          value={userPhoneNum}
+          onChange={(e) => setUserPhoneNum(e.target.value)}
+        />
+        {userPhoneNum.length > 1 && userPhoneNum.length < 11 ? (
+          <p className="error_p">invalid phone number</p>
+        ) : (
+          <p className={`error_p ${noNumber ? "" : "err"}`}>
+            {userPhoneNum < 11
+              ? "invalid phone number"
+              : " phone number is required"}
+          </p>
+        )}
 
-      <button type="submit">submit</button>
-    </form>
+        <button type="submit">Continue</button>
+      </form>
+    </>
+  );
+}
+
+function RegistrationLogo() {
+  return (
+    <>
+      <div className="logo">
+        <img src="/korir.jpg" className="logo-img" alt="korir" />
+      </div>
+      <h3 className="logo_heading">Create account at korir Ecommerce</h3>
+    </>
   );
 }
