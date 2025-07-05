d757a5158dc39132ac16fc40941a336e02be7f45
Iamkorir3 <korir5956@gmail.com>
2025-07-05 16:40:41 +0300

    finished setting nav UI

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,3 +1,51 @@
 export default function Navigation() {
-  return <div>Nav</div>;
+  return (
+    <div className="nav_container">
+      <button className="nav_btn">
+        <ion-icon name="menu-outline"></ion-icon>
+      </button>
+      <Logo />
+      <SearchBar />
+      <Profile />
+      <Cart />
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div>
+      <h2>ECOMMERCE⭐</h2>
+    </div>
+  );
+}
+
+function SearchBar() {
+  return (
+    <div className="search">
+      <input type="text" placeholder="men's clothings" value={null} />
+      <button>Search</button>
+    </div>
+  );
+}
+
+function Profile() {
+  return (
+    <div>
+      <button className="nav_btn">
+        {" "}
+        <ion-icon name="person-outline"></ion-icon> <span>Account</span>
+      </button>
+    </div>
+  );
+}
+
+function Cart() {
+  return (
+    <div>
+      <button className="nav_btn">
+        <ion-icon name="cart-outline"></ion-icon> <span>Cart</span>
+      </button>
+    </div>
+  );
 }
